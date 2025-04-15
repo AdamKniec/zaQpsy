c6d6d4a014d6a46df755c865884efb79e3a6c023
AdamKniec <adam.k.kniec@gmail.com>
2025-04-15 21:16:25 +0200

    adding some basic styles and code improvements

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,70 @@
-import { Text, View, TextInput } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  SafeAreaView,
+  Button,
+} from "react-native";
+import { FlatList } from "react-native";
+import { ListItem } from "./components/ListItem/ListItem";
 
-export default function Index() {
+const Index = () => {
+  // TODO fix the TS once we create the proper product object
+  const [listItems, setListItems] = useState<string[]>([]);
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleButtonPress = () => {
+    setListItems((prevState) => {
+      return [...prevState, inputValue];
+    });
+    setInputValue("");
+  };
+  const handleInputChange = (text: string) => {
+    setInputValue(text);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <TextInput placeholder="Dodaj produkt" />
-    </View>
+    <SafeAreaView style={RootPageStyles.root}>
+      <View style={{ gap: "10px" }}>
+        <FlatList
+          data={listItems}
+          scrollEnabled
+          style={{ height: "90%" }}
+          ItemSeparatorComponent={() => {
+            return <View style={{ height: 16 }} />;
+          }}
+          renderItem={(product) => {
+            return <ListItem productName={product.item} />;
+          }}
+        ></FlatList>
+      </View>
+
+      <View>
+        <TextInput
+          placeholder="Dodaj produkt"
+          style={{ color: "white" }}
+          value={inputValue}
+          placeholderTextColor={"grey"}
+          onChangeText={handleInputChange}
+        />
+        <Button title="Dodaj" onPress={handleButtonPress} />
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default Index;
+
+const RootPageStyles = StyleSheet.create({
+  root: {
+    flex: 1,
+    borderWidth: 1,
+    width: "100%",
+    backgroundColor: "#201b4a",
+    padding: 20,
+    paddingBottom: 40,
+    justifyContent: "space-between",
+  },
+});
