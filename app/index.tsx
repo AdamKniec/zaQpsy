7fdbd5cc377783de29afcbcd546c3194585d66e4
AdamKniec <adam.k.kniec@gmail.com>
2025-04-24 20:46:29 +0200

    change the structure of the product and install uuid library

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,82 @@
-import { Text, View, TextInput } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  SafeAreaView,
+  Button,
+} from "react-native";
+import { FlatList } from "react-native";
+import { ListItem } from "./components/ListItem/ListItem";
+import { v4 as uuidv4 } from "uuid";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <TextInput placeholder="Dodaj produkt" />
-    </View>
-  );
+interface Product {
+  name: string;
+  id: string;
 }
+
+const Index = () => {
+  const [listItems, setListItems] = useState<Product[]>([]);
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleButtonPress = () => {
+    setListItems((prevState) => {
+      return [
+        ...prevState,
+        {
+          id: uuidv4(),
+          name: inputValue,
+        },
+      ];
+    });
+    setInputValue("");
+  };
+  const handleInputChange = (text: string) => {
+    setInputValue(text);
+  };
+
+  return (
+    <SafeAreaView style={RootPageStyles.root}>
+      <View style={{ gap: "10px" }}>
+        <FlatList
+          data={listItems}
+          scrollEnabled
+          style={{ height: "90%" }}
+          ItemSeparatorComponent={() => {
+            return <View style={{ height: 16 }} />;
+          }}
+          renderItem={(product) => {
+            console.log("product", product);
+            return <ListItem productName={product.item.name} />;
+          }}
+        ></FlatList>
+      </View>
+
+      <View>
+        <TextInput
+          placeholder="Dodaj produkt"
+          style={{ color: "white" }}
+          value={inputValue}
+          placeholderTextColor={"grey"}
+          onChangeText={handleInputChange}
+        />
+        <Button title="Dodaj" onPress={handleButtonPress} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Index;
+
+const RootPageStyles = StyleSheet.create({
+  root: {
+    flex: 1,
+    borderWidth: 1,
+    width: "100%",
+    backgroundColor: "#201b4a",
+    padding: 20,
+    paddingBottom: 40,
+    justifyContent: "space-between",
+  },
+});
