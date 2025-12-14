5f5f5af9455b35fc31443214068bbb2f7638579f
AdamKniec <adam.k.kniec@gmail.com>
2025-12-14 18:38:14 +0100

    detect internet connectivity mechanism

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,37 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Tile from "./components/Tile/Tile";
 import paths from "./routing";
+import { useNetInfo } from "@react-native-community/netinfo";
 
 const Index = () => {
+  const { isConnected } = useNetInfo();
+
+  if (!isConnected) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: "#fff",
+            fontSize: 36,
+            borderBlockColor: "#fff",
+            borderWidth: 1,
+            borderColor: "#fff",
+            padding: 10,
+          }}
+        >
+          Włącz internet!
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View style={Styles.wrapper}>
       <Tile label="Produkty" path={paths.products} />
