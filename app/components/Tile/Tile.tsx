ed299853bb2e00dfd6e6eb0997b80bf0257bc0fc
AdamKniec <adam.k.kniec@gmail.com>
2025-12-15 21:06:34 +0100

    partial implementation of images on tiles

--- app/components/Tile/Tile.tsx
+++ app/components/Tile/Tile.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "expo-router";
 import { Pressable, Text, View } from "react-native";
 import Styles from "./Tile.styles";
+import { Image } from "expo-image";
+import ShoppingCartIcon from "../../../assets/images/shopping-cart.png";
 
 interface TileProps {
   label: string;
@@ -14,6 +16,7 @@ const Tile = (props: TileProps) => {
     // TODO FIX TS ISSUE
     <Pressable onPress={() => router.navigate(props.path)} style={Styles.main}>
       <View>
+        <Image source={ShoppingCartIcon} style={{ width: 100, height: 100 }} />
         <Text style={{ textAlign: "center", color: "#f6f7f8" }}>
           {props.label}
         </Text>
