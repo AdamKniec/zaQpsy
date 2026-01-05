b1a0edf74f7c99f5d89c7dccadbfda74e93867cc
AdamKniec <adam.k.kniec@gmail.com>
2026-01-05 19:50:03 +0100

    refactor the add product form

--- app/api/products/useAddProduct.ts
+++ app/api/products/useAddProduct.ts
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import api from "../api";
+import { queryClient } from "@/app/_layout";
 
 const addProductRequest = async (newProduct: any) => {
   const response = await api.post("/products", {
@@ -11,6 +12,9 @@ const addProductRequest = async (newProduct: any) => {
 const useAddProducts = () => {
   const { mutate: addProduct } = useMutation({
     mutationFn: addProductRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+    },
   });
 
   return {
--- app/api/products/useDeleteProducts.ts
+++ app/api/products/useDeleteProducts.ts
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import api from "../api";
+import { queryClient } from "@/app/_layout";
 
 const deleteProductRequest = async (id: string) => {
   await api.delete(`/products?id=eq.${id}`);
@@ -8,6 +9,9 @@ const deleteProductRequest = async (id: string) => {
 const useDeleteProduct = () => {
   const { mutate: deleteProduct } = useMutation({
     mutationFn: deleteProductRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+    },
   });
 
   return { deleteProduct };
