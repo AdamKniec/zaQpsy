09defb3c61ac6f282eeb3e437c1f0ae82dc6e646
AdamKniec <adam.k.kniec@gmail.com>
2026-01-14 19:00:19 +0100

    small css and ts improvements

--- app/components/Tile/Tile.tsx
+++ app/components/Tile/Tile.tsx
@@ -1,11 +1,11 @@
-import { useRouter } from "expo-router";
+import { Href, useRouter } from "expo-router";
 import { ImageSourcePropType, Pressable, Text, View } from "react-native";
 import Styles from "./Tile.styles";
 import { Image } from "expo-image";
 
 interface TileProps {
   label: string;
-  path: "/expenses" | "/products";
+  path: Href;
   imgSrc: ImageSourcePropType;
 }
 
@@ -13,7 +13,6 @@ const Tile = (props: TileProps) => {
   const router = useRouter();
 
   return (
-    // TODO FIX TS ISSUE
     <Pressable onPress={() => router.navigate(props.path)} style={Styles.main}>
       <View style={Styles.contentWrapper}>
         <Image source={props.imgSrc} style={Styles.image} />
