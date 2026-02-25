48f3ef96c33f2ebad83498e9d7194a2ff9673392
AdamKniec <adam.k.kniec@gmail.com>
2026-02-25 19:36:11 +0100

    unused-vars plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
-// https://docs.expo.dev/guides/using-eslint/
+// eslint.config.js
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
+const unusedImports = require("eslint-plugin-unused-imports");
 
 module.exports = defineConfig([
   expoConfig,
@@ -8,13 +9,21 @@ module.exports = defineConfig([
     ignores: ["dist/*"],
   },
   {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+
     rules: {
-      "no-unused-vars": 1,
-      "@typescript-eslint/no-unused-vars": [
-        "error", {
-          varsIgnorePattern:"^_",
-          argsIgnorePattern:"^_"
-        }
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+        },
       ],
     },
   },
